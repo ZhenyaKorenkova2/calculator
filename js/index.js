9a2017f293e5a09d0adddb2db3a054349542d53a
ZhenyaKorenkova2 <korenkovazhenya2@gmail.com>
2020-02-12 14:20:42 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
 const display = document.querySelector('.display');
 
+document.querySelector(".clear").addEventListener('click', clear);
+function clear(CE) {
+    display.value = null
+}
+
 document.querySelectorAll('.digits button')
     .forEach( button => button.addEventListener('click', digitsPressed));
-
 function digitsPressed(ev) {
     display.value += ev.target.innerText;
 }
 
 document.querySelectorAll('.opers button')
     .forEach( button => button.addEventListener('click', opersPressed));
-
 function opersPressed(ev) {
     display.value += ev.target.innerText;
 }
 
-document.querySelector('.eq').addEventListener('click', calculate);
 
+document.querySelector('.eq').addEventListener('click', calculate);
 function calculate() {
     display.value = eval(display.value);
 }
